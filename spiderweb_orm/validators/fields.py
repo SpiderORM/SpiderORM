695f92410242b300203ed926e32bdb55556f74d8
simondev413 <devemail413@gmail.com>
2024-07-24 14:39:41 +0000

    feat: created the validate_email method for verify the email pattern of EmailField values.

--- spiderweb_orm/validators/fields.py
+++ spiderweb_orm/validators/fields.py
@@ -1,7 +1,9 @@
-from spiderweb_orm.validators.exceptions import ValidationError
 from decimal import Decimal
 from datetime import date,datetime
-import re
+
+from spiderweb_orm.validators.exceptions import ValidationError
+from spiderweb_orm.validators._re import verify_email_pattern,verify_url_pattern
+
 
 def validate_required(value):
     if value is None or value == '':
@@ -13,17 +15,17 @@ def validate_null(value):
 
 def validate_string(value,max_length=None):    
     if not isinstance(value,str):
-        raise ValidationError(f" Value must be a string: {value}")
+        raise ValidationError(f" Value must be a string: {value}.")
     if max_length and len(value) > max_length:
-        raise ValidationError(f"Value exceeds maximun length of {max_length}")
+        raise ValidationError(f"Value exceeds maximun length of {max_length}.")
     
 def validate_integer(value):
     if not isinstance(value, int):
-        raise ValidationError(f"Value must be an integer: {value}")
+        raise ValidationError(f"Value must be an integer: {value}.")
 
 def validate_float(value):
     if not isinstance(value,float):
-        raise ValidationError(f"Value must be a float: {value}")
+        raise ValidationError(f"Value must be a float: {value}.")
 
 def validate_decimal(value):  
     
@@ -35,36 +37,35 @@ def validate_decimal(value):
             
 def validate_boolean(value):
     if not isinstance(value,bool):
-        raise ValidationError(f"Value must be a boolean: {value}")
+        raise ValidationError(f"Value must be a boolean: {value}.")
     
 def validate_date(value):
     if not isinstance(value,date):
-        raise ValidationError(f"Value must be a date: {value}")
+        raise ValidationError(f"Value must be a date: {value}.")
     
 def validate_datetime(value):
     if not isinstance(value,datetime):
-        raise ValidationError(f"Value must be a datetime: {value}")
+        raise ValidationError(f"Value must be a datetime: {value}.")
 
 def validate_choices(value,choices):
     if value not in choices:
-        raise ValidationError(f"Value must be one of {choices}")
+        raise ValidationError(f"Value must be one of {choices}.")
 
 def validate_url(value):
-    url_regex = re.compile(
-        r'^(?:http|ftp)s?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'
-        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$',re.IGNORECASE
-    )
-    if not re.match(url_regex,value):
-        raise ValidationError(f"Value must be a valid URL: {value}")
-
+    value = verify_url_pattern(value)
+    if not value:
+        raise ValidationError(f"Value must be a url.")
+   
+    
 def validate_file_type(value,allowed_types):
     if not any(value.endswith(allowed_type) for allowed_type in allowed_types):
-        raise ValidationError(f"Value must be one of {allowed_types}")
+        raise ValidationError(f"Value must be one of {allowed_types}.")
     
 def validate_default(value,default):
     return value if value is not None else default
+
+def validate_email(value):
+    value = verify_email_pattern(value)
+    if not value:
+        raise ValidationError(f"Value don't match with the email pattern.")
+    
