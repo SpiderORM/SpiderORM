1cb59258545ce233af60c78feefdeddf65235885
Simão Domingos de Oliveira António <devemail413@gmail.com>
2024-08-13 01:47:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spider-orm",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
