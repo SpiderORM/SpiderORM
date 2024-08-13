b35148089fcc4668b0e531a735371d72f5844f30
Simão Domingos de Oliveira António <devemail413@gmail.com>
2024-08-13 11:17:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'mysqlclient',
-        'sqlite3',  
+        'mysqlclient', 
     ],
     extras_require={
         'dev': [
